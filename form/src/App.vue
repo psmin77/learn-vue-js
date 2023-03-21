a05659f753327e7c73e6dd6ac06e8fba2bcc35fd
Seongmin <chr92@naver.com>
2023-03-21 18:05:48 +0900

    [Section12] final test

---
+++ form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username"> ID : </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password"> PW : </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username, this.password);
+
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+
+      axios.post(url, data)
+           .then(resp => {
+              console.log(resp);
+           })
+           .catch(error => {
+              console.log(error);
+           })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
